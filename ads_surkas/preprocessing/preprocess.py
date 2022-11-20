051d8906b55ab279c36b8c62fe6b8e7371c573ac
Ulrik Røsby <ulrik@rosby.no>
2022-11-20 03:03:06 +0100

    feat: add more featurized tables

--- ads_surkas/preprocessing/preprocess.py
+++ ads_surkas/preprocessing/preprocess.py
@@ -22,4 +22,31 @@ def clean_data(df: pd.DataFrame):
     data["Month"] = data["Month"].astype("int32")
     data["Day"] = 1
     data["Time"] = pd.to_datetime(data[["Year", "Month", "Day"]])
-    return data
+    data["Customer Since"] = pd.to_datetime(data["Customer Since"])
+    dat = data.filter(
+        [
+            "item_id",
+            "status",
+            "created_at",
+            "sku",
+            "Customer ID",
+            "increment_id",
+            "category_name_1",
+            "sales_commission_code",
+            "discount_amount",
+            "payment_method",
+            "Working Date",
+            "BI Status",
+            "MV",
+            "Customer Since",
+            "M-Y",
+            "FY",
+            "price",
+            "qty_ordered",
+            "grand_total",
+            "Time",
+            "Year",
+            "Month",
+        ]
+    )
+    return dat[~((dat["grand_total"] < 0) & (dat["status"] == "complete"))]
